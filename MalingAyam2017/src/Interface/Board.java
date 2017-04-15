2667766bb7efba4715c3cef4c98f3a0b846529cb
rizkyfaramita <rizky.faramitha@gmail.com>
2017-04-15 09:11:02 +0700

    coba2 maling ayam tp blm selesai

---
+++ MalingAyam2017/src/Interface/Board.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author Rizky Faramita
+ */
+public class Board {
+
+    
+    public Board() {
+        //implementasi board selain GameBoard
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+
+    }
+
+    private void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
---
+++ MalingAyam_README.txt
@@ -0,0 +1,11 @@
+1. ini adaptasi dari http://zetcode.com/tutorials/javagamestutorial/pacman/
+2. semua ghost diubah jadi police, cuma ada 1 police
+3. semua pacman diubah jadi thief, cuma ada 1 thief tp punya 3 nyawa
+4. bisa lanjut ke level berikutnya
+5. To-do list:
+	a. method2 yg berhubungan sama police di GameBoard.java dipindahin ke Police.java
+	b. method2 yg berhubungan sama thief di GameBoard.java dipindahin ke Thief.java
+	c. modifikasi method2 police supaya pas sejajar sama thief speednya nambah
+	d. gabungan GameBoard dengan board2 lainnya (board2 yg sudah diimplementasikan sm diar sebelumnya) di dalam Board.java
+	e. ganti police.gif & thief.gif jadi police.png & thief.png dengan gambar yang sesuai
+	f. bikin unit testnya
